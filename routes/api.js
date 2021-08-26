a4bf846437e325e8405c4d335e7432fee9fcbd7a
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-08-26 15:28:57 -0400

    api work

---
+++ routes/api.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const Transaction = require("../models/transaction.js");
+
+router.post("/api/transaction", ({body}, res) => {
+    Transaction.create(body)
+    .then(dbTransaction => {
+        res.json(dbTransaction);
+    })
+    .catch(err => {
+        res.status(404).json(err);
+    });
+});
+
+router.post("/api/transaction/bulk", ({body}, res)=> {
+    Transaction.insertMany(body)
+        .then(dbTransaction => {
+            res.json(dbTransaction);
+        })
+})
